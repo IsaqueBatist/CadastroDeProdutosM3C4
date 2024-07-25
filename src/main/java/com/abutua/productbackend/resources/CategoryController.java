90c0b1b5df167ed805ff97bcef84f6e31c9c522f
Isaque <isaquebatista400@gmail.com>
2024-07-25 14:39:30 -0300

    Configurando endpoints GET/id e POST para interagir com o BD

--- src/main/java/com/abutua/productbackend/resources/CategoryController.java
+++ src/main/java/com/abutua/productbackend/resources/CategoryController.java
@@ -3,9 +3,13 @@ package com.abutua.productbackend.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.productbackend.models.Category;
 import com.abutua.productbackend.repositories.CategoryRepository;
@@ -14,23 +18,16 @@ import com.abutua.productbackend.repositories.CategoryRepository;
 @CrossOrigin
 public class CategoryController {
 
-    // private List<Category> categories = Arrays.asList(new Category(1, "Produção Própria"),
-    //         new Category(2, "Nacional"),
-    //         new Category(3, "Importado"),
-    //         new Category(4, "Premium"));
-
     @Autowired
     private CategoryRepository categoryRepository;
 
-    // @GetMapping("categories/{id}")
-    // public ResponseEntity<Category> getCategory(@PathVariable int id) {
-    //     Category cat = categories.stream()
-    //             .filter(p -> p.getId() == id)
-    //             .findFirst()
-    //             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+    @GetMapping("categories/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable int id) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
-    //     return ResponseEntity.ok(cat);
-    // }
+        return ResponseEntity.ok(category);
+    }
 
     @GetMapping("categories")
     public List<Category> getCategories() {
--- src/main/java/com/abutua/productbackend/resources/ProductController.java
+++ src/main/java/com/abutua/productbackend/resources/ProductController.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.productbackend.models.Product;
@@ -19,13 +22,14 @@ import com.abutua.productbackend.repositories.ProductRepository;
 @RestController
 @CrossOrigin
 public class ProductController {
-
-    private List<Product> products = new ArrayList<>();
-
+    
+    @Autowired
+    private ProductRepository productRepository;
+    
     @PostMapping("products")
     public ResponseEntity<Product> save(@RequestBody Product product) {
-        product.setId(products.size() + 1);
-        products.add(product);
+
+        product = productRepository.save(product);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -37,19 +41,13 @@ public class ProductController {
 
     }
 
-    // @GetMapping("products/{id}")
-    // public ResponseEntity<Product> getProduct(@PathVariable int id) {
-    // Product prod = products.stream()
-    // .filter(p -> p.getId() == id)
-    // .findFirst()
-    // .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product
-    // not found"));
+    @GetMapping("products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
 
-    // return ResponseEntity.ok(prod);
-    // }
+        return ResponseEntity.ok(product);
+    }
 
-    @Autowired
-    private ProductRepository productRepository;
 
     @GetMapping("products")
     public List<Product> getProducts() {
